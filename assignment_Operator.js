37e3a3d862065d556f13ed914c4f0d4caee23dbe
DarshanMistari <mistaridarshan2@gmail.com>
2024-06-21 11:50:21 +0530

    [ADDED]:Darshan Javascript Assigment OPerator program

---
+++ assignment_Operator.js
@@ -0,0 +1,23 @@
+// Assignment Operator
+
+let num1 = 5;
+let num2 = 8;
+
+console.log("Assignment Operator.");
+
+console.log("num1 = 5 , num2 = 8");
+
+num1 += 5;
+console.log("num1 assigne 5 :",num1);
+
+num2 -= 2;
+console.log("num2 minus num1 :",num2)
+
+num1 *= num1;
+console.log("num1 multiplay num1 :",num1);
+
+num2 /= 2;
+console.log("num2 divide 4 :",num2)
+
+num2 %= 2;
+console.log("num2 rem 2 :",num2);
