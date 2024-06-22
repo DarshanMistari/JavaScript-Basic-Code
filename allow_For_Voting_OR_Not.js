8c909ecb942e441bf868cabd2e1eeed5aa51188a
DarshanMistari <mistaridarshan2@gmail.com>
2024-06-22 10:56:11 +0530

    [ADDED]:Darshan Javascript code allow for voting or n

---
+++ allow_For_Voting_OR_Not.js
@@ -0,0 +1,12 @@
+// Allow for Voting Or Not 
+
+let age = 18;
+
+if(age >= 18)
+    {
+        console.log("You are Allow for Voting.");
+    }
+    else
+    {
+        console.log("You are not Allow for Voting.")
+    }
