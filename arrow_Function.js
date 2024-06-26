731c30eefbcd827aef304970350cadf48bd6593d
DarshanMistari <mistaridarshan2@gmail.com>
2024-06-26 15:04:44 +0530

    [ADDED]:Darshan Javascript code arrow Function

---
+++ arrow_Function.js
@@ -0,0 +1,16 @@
+// arrow Function
+
+const arithmaticOper = (a , b) => {
+
+    console.log("Addition :", a + b);
+    console.log("Subtraction :", a - b);
+    console.log("Multiplication :", a * b);
+    console.log("Division :", a / b);
+
+};
+
+let arithmaticOperation = arithmaticOper(15,9);
+
+console.log(arithmaticOperation);
+
+
