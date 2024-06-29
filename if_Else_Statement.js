6a729867ca5642bcbc04d2239edf32baf6bf6983
DarshanMistari <mistaridarshan2@gmail.com>
2024-06-29 22:33:05 +0530

    [ADDED]:Darshan Javascript code if else statement

---
+++ if_Else_Statement.js
@@ -0,0 +1,12 @@
+// IF Else Statement 
+
+let age = 20;
+
+if(age >= 18)
+    {
+        console.log("You are allow for Driving Licences.");
+    }
+    else
+    {
+        console.log("You are Not allow for Driving Licences.");
+    }
