e5fb6b2a3092596a2ff089a1df06ae2d884f8e97
DarshanMistari <mistaridarshan2@gmail.com>
2024-06-30 08:55:40 +0530

    [ADDED]:Darshan Javascript codelogical operator

---
+++ logical_Operator.js
@@ -0,0 +1,20 @@
+// Logical Operator
+
+let num1 = 8;
+let num2 = 7;
+
+
+console.log("Logical Oprator.");
+
+console.log("num1 = 8 , num2 = 7");
+console.log("Logical AND && Oprator.");
+
+console.log("(num1 >= 7) && (num2 <= 8) :",(num1 >= 7) && (num2 <= 8));
+
+console.log("Logical OR || Oprator.");
+
+console.log("(num1 >= 9) || (num2 <= 5) :",(num1 >= 9) || (num2 <= 5));
+
+console.log("Logical NOT ! Oprator.");
+
+console.log("!(num1 == num2) :",(!(num1 == num2)));
