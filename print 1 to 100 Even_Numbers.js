a6fbd9b7a4f895daa0b8f9d590555b64792b949c
DarshanMistari <mistaridarshan2@gmail.com>
2024-07-03 22:36:56 +0530

    [ADDED]:Darshan Javascript code print 1 to 100 even numbers

---
+++ print 1 to 100 Even_Numbers.js
@@ -0,0 +1,13 @@
+// Print 1 to 100 Even Numbers
+
+let num = 100;
+let i =1;
+
+console.log("1 to 100 Even Numbers \n")
+for(i = 1; i <= num; i++)
+    {
+        if(i % 2 == 0)
+            {
+                console.log(i);
+            }
+    }
