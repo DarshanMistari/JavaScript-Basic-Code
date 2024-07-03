5fb88f12034b2fce25b431daf08a1e5ef88bb67d
DarshanMistari <mistaridarshan2@gmail.com>
2024-07-03 22:38:11 +0530

    [ADDED]:Darshan Javascript code

---
+++ number_MultiplayBy 7 OR_Not.js
@@ -0,0 +1,14 @@
+//  Number is Multiplay By 7 Or Not 
+
+let num = prompt("Enter the Number.");
+
+console.log("Number is Multiplay By 7 or Not.")
+
+if(num % 7 == 0)
+    {
+        console.log(num,"is Multiplay By 7.");
+    }
+    else
+    {
+        console.log(num,"is Not Multiplay By 7");
+    }
