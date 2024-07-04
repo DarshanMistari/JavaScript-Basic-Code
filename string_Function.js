90b7e8dad946d0814a3de00cc79fd07bcc7282fc
DarshanMistari <mistaridarshan2@gmail.com>
2024-07-04 19:27:02 +0530

    [ADDED]:Darshan Javascript code String Function

---
+++ string_Function.js
@@ -0,0 +1,18 @@
+// String Function in Javascript
+
+let str1 = "Javascript";
+let str2 = "Java";
+let str3 = "  Welcome   "
+console.log("str toUpperCase :",str1.toUpperCase());
+
+console.log("str2 toLowerCase :",str2.toLowerCase());
+
+console.log("str1 and str2 concate :",str1.concat(str2));
+
+console.log("str1 replace with Welcome :",str1.replace("Javascript","Welcome"));
+
+console.log("str3 remove starting and ending space :",str3.trim());
+
+console.log("str1 slicing :",str1.slice(4,9));
+
+console.log("str1 CharAt index of character :",str1.charAt(0));
