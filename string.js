174bb88f6693445f09ab18d1030037b94f5a3506
DarshanMistari <mistaridarshan2@gmail.com>
2024-07-04 19:25:04 +0530

    [ADDED]:Darshan Javascript code String

---
+++ string.js
@@ -0,0 +1,10 @@
+// String in Javascript
+
+let str = 'Hello EveryOne';
+let str2 = "Welcome to Javascript";
+
+console.log(str);
+console.log(str2);
+
+let str3 = str.length;
+console.log("Length of str :",str3)
