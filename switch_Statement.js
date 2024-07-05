0b5eed0d574fa2bd4bf40546057723373fbb9f2c
DarshanMistari <mistaridarshan2@gmail.com>
2024-07-05 22:31:07 +0530

    [ADDED]:Darshan Javascript code switch statement

---
+++ switch_Statement.js
@@ -0,0 +1,23 @@
+// Switch Statement in javascript
+
+let fruits = "Grapes";
+
+switch("Grapes")
+{
+    case "Grapes":
+        console.log("I like a Grapes.");
+        break;
+        
+        case "Mango":
+            console.log("I like a Mango.");
+            break;
+
+            case "Orange":
+                console.log("I like a Orange.");
+                break;
+
+                default:
+                    console.log("Invalide Fruits Name.");
+                    break;
+
+}
