2c6e14490a2d48fbc4c76a73ae3060dcb543bb49
DarshanMistari <mistaridarshan2@gmail.com>
2024-07-07 21:24:53 +0530

    [ADDED]:Darshan Javascript code while loop

---
+++ while_Loop.js
@@ -0,0 +1,12 @@
+// print the Even Numbers 1 to 10
+let i = 1;
+
+console.log("Even Numbers :")
+while(i <= 10)
+    {
+        if(i % 2 == 0)
+            {
+                console.log(i);
+            }
+        i +=1;    
+    }
