2f56de5d89464488a169a4b28bca5443bfdc188c
DarshanMistari <mistaridarshan2@gmail.com>
2024-07-09 16:00:51 +0530

    [ADDED]:Darshan Javascript code dom Prperty

---
+++ dom_Property.js
@@ -0,0 +1,11 @@
+let names = document.querySelectorAll(".name");
+console.log(names);
+
+let h3 =document.querySelector("h3");
+
+h3.innerText = h3.innerText + "My name is Darshan";
+
+let tag = document.getElementsByTagName("h3");
+console.log(tag);
+
+console.log(names.innerHtml);
