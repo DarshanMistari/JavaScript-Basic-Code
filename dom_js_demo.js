6827b90d766fe11e5e8ae460657a06c84e10c214
DarshanMistari <mistaridarshan2@gmail.com>
2024-07-09 16:02:43 +0530

    [ADDED]:Darshan Javascript code dom demo

---
+++ dom_js_demo.js
@@ -0,0 +1,8 @@
+let head = document.getElementsByClassName(".heading");
+console.log(head);
+
+let para = document.getElementById("#para");
+console.log(para);
+
+let tag = document.getElementsByTagName("h3");
+console.log(tag);
