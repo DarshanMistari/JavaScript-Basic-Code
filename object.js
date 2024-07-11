9d283c325567699d851ea30476f4941be784960d
DarshanMistari <mistaridarshan2@gmail.com>
2024-07-11 23:24:18 +0530

    [ADDED]:Darshan Javascript code OBJECT

---
+++ object.js
@@ -0,0 +1,9 @@
+const Students = {
+    Name : "Darshan Mistari",
+    marsk : "80.85",
+    Age : 20,
+};
+
+console.log("Javascript object : ",Students);
+
+console.log("Name :",Students["Name"]);
