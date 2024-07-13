7fee1f74e568f83c8d390fa628450a051d1fdda1
DarshanMistari <mistaridarshan2@gmail.com>
2024-07-13 19:29:06 +0530

    [ADDED]:Darshan Javascript code exception handling

---
+++ exception_Handling.js
@@ -0,0 +1,18 @@
+// Exception Handling Example
+
+let num1 = 45;
+let num2 = 55;
+
+console.log("Num1 :",num1);
+console.log("Num2 :",num2);
+
+try{
+    console.log("Sum :",num1 + a);
+}
+catch(err){
+    console.log(err);
+}
+
+console.log("Addition :",num1 + num2)
+
+
